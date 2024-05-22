9fc8df140b4be3be306cee0721a170e29d5b1824
Raghad <s12219053@stu.najah.edu>
2024-05-22 23:58:03 +0300

    Add back folder

---
+++ backend/models/userModel.js
@@ -0,0 +1,24 @@
+// userModel.js
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    userName: { type: String, required: true, trim: true },
+    email: { type: String, required: true, unique: true, trim: true },
+    phone: { type: String, trim: true },
+    password: { type: String, required: true },
+    isAdmin: { type: Boolean, default: false },
+    birthdayDate: { type: Date },
+    favPro: [{ type: Schema.Types.ObjectId, ref: 'Product' }],
+    cart: [
+        {
+            product: { type: Schema.Types.ObjectId, ref: 'Product' },
+            quantity: { type: Number, min: 1, required: true }
+        }
+    ],
+    customProduct: [{ type: Schema.Types.ObjectId, ref: 'Product' }]
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
